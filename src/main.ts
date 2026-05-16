6c0a56e9c271d4cd049f60b2be5367f0dcf585c6
2iaad <ziadderfoufi77@gmail.com>
2026-05-16 04:55:02 +0100

    feat(auth): enhance authentication with DTOs and validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // global convention
+  app.useGlobalPipes(new ValidationPipe()); // enable validation globally -  bridge between the raw request and your DTO | Without it, NestJS ignores your DTO decorators completely.
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

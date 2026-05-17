8694d8d5e21c6cdcb12c1295c15d2105a86e8c66
2iaad <ziadderfoufi77@gmail.com>
2026-05-17 04:41:40 +0100

    feat(env): enhance environment configuration with validation and update .env.example

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { ConfigService } from '@nestjs/config';
+import { Env } from 'src/config/env.validation';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly config: ConfigService) {}
+    constructor(private readonly config: ConfigService<Env>) {} // <Env> so infer knows the type
 
     register(body: RegisterDto) {
-        const db_url = this.config.get<string>('DB_URL');
+        const port = this.config.get('PORT', { infer: true }); // infer: true -> return type from the schema instead
 
-        console.log(db_url);
+        console.log(port);
         // TODO: check if user already exists with that email
         // TODO: check if mail is valide + exists
         // TODO: hash password with salt + save user in db and return jwt

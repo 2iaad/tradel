cb721a4f4dca8e5e8d9c141dcb306053d509651d
2iaad <ziadderfoufi77@gmail.com>
2026-05-24 15:29:49 +0100

    feat: add Logger()

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        // logger: false,
+    });
+    const logger = new Logger(AppModule.name);
 
     app.setGlobalPrefix('api'); // global convention for backend
     app.useGlobalPipes(new ValidationPipe()); // enable validation, bridge between the raw request and your DTO | Without it, NestJS ignores your DTO decorators completely.
 
     await app.listen(process.env.PORT ?? 3000);
+    logger.log('Server running on port: ' + 3000);
 }
 bootstrap();

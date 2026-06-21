c440f2699c467ad8c57f4167ea039d87b9aef0c9
2iaad <ziadderfoufi77@gmail.com>
2026-06-21 23:27:08 +0100

    feat(auth): implement JWT authentication with user registration and login functionality

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { UsersRepository } from 'src/users/users.repository';
+
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import type { Env } from 'src/config/env.validation';
+
+import { AuthController } from './auth.controller'; // controllers
+
+import { AuthService } from './auth.service'; // providers
+import { UsersRepository } from 'src/users/users.repository'; // providers
 
 @Module({
+    imports: [
+        JwtModule.registerAsync({
+            // TODO: understand more this part
+            inject: [ConfigService],
+            useFactory: (config: ConfigService<Env>) => ({
+                secret: config.get('JWT_ACCESS_SECRET', { infer: true }),
+                signOptions: {
+                    expiresIn: config.get('JWT_ACCESS_TTL', { infer: true }),
+                },
+            }),
+        }),
+    ],
+
     controllers: [AuthController], // handles GET /users, POST /users, etc.
     providers: [AuthService, UsersRepository], // business logic
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,44 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { ConfigService } from '@nestjs/config';
 import { Env } from 'src/config/env.validation';
+import { UsersRepository } from 'src/users/users.repository';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     private readonly logger = new Logger(AuthService.name); // checked only at compile-time
-    constructor(private readonly config: ConfigService<Env>) {} // <Env> so infer knows the type
 
-    register(body: RegisterDto) {
-        const port = this.config.get('PORT', { infer: true }); // infer: true -> return type from the schema instead
+    constructor(
+        private readonly users: UsersRepository,
+        private readonly jwt: JwtService,
+        private readonly config: ConfigService<Env>,
+    ) {} // <Env> so infer knows the type
 
-        this.logger.log(port);
-        // TODO: check if user already exists with that email
-        // TODO: check if mail is valide + exists
-        // TODO: hash password with salt + save user in db and return jwt
+    async register(body: RegisterDto) {
+        const passwordHash: string = await bcrypt.hash(body.password, 12);
+        const user = await this.users.create(
+            body.username,
+            body.email,
+            passwordHash,
+        );
+        this.logger.log(`Registered user: ${user.email}`);
+        // TODO: return jwt
         return body;
     }
 
-    login(body: LoginDto) {
-        // TODO: check if user exist + return jwt
+    async login(body: LoginDto) {
+        const user = await this.users.findByEmail(body.email);
+        if (
+            !user ||
+            (await bcrypt.compare(body.password, user.password_hash))
+        ) {
+            throw new UnauthorizedException('Invalide credentials');
+        }
+
+        // TODO: return jwt
         return body;
     }
 }

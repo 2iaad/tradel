678462a3e52cbaf3122ad58a4bba4a3a2d24cf3e
2iaad <ziadderfoufi77@gmail.com>
2026-06-21 12:37:03 +0100

    feat(migrations): implement database migrations using node-pg-migrate
    - Added a new documentation file for database migrations outlining the migration process and usage of node-pg-migrate.
    - Updated .env.example to include DB_URL for database connection.
    - Created initial migration for users table with up and down migration scripts.
    - Added node-pg-migrate as a development dependency in package.json and package-lock.json.
    - Updated npm scripts in package.json for migration commands.
    - Registered DB_URL in environment validation schema to ensure proper configuration.

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -14,6 +14,7 @@ const envSchema = z.object({
     DB_PORT: z.coerce.number().int().positive().default(5432), // coerce -> convert from string to number
     DB_HOST: z.string().min(1),
     DB_DATA: z.string().min(1),
+    DB_URL: z.url(),
     // jwt
     JWT_ACCESS_SECRET: z.string().min(32),
     JWT_REFRESH_SECRET: z.string().min(32),

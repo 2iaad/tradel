373d7426f072e54222239ab19da3ec0517e45859
2iaad <ziadderfoufi77@gmail.com>
2026-07-11 04:17:16 +0100

    [feat(trades)]: implement Trades API & module with CRUD operations and DTOs
    closes #16

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
     app.setGlobalPrefix('api'); // global convention for backend
     app.use(cookieParser()); // set the req.cookies
-    app.useGlobalPipes(new ValidationPipe()); // enable dto validation one request body
+    app.useGlobalPipes(new ValidationPipe({ transform: true })); // validate + run @Transform on dto
     app.enableCors({
         origin: 'http://localhost:5173',
         credentials: true, // necessary for the browser to send/receive cookies

3bb67c339abcb8f2991555e1d2d70f3c2bbf399d
2iaad <ziadderfoufi77@gmail.com>
2026-07-11 20:19:38 +0100

    feat: integrate @nestjs/swagger for API documentation and update DTO imports

--- backend/src/trades/dto/update-trade.dto.ts
+++ backend/src/trades/dto/update-trade.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { IsNumber, IsOptional } from 'class-validator';
 import { CreateTradeDto } from './create-trade.dto';
 

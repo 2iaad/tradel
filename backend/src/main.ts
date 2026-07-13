15ce6a0209937f0e267918c08f0a00deb67b6863
2iaad <ziadderfoufi77@gmail.com>
2026-07-13 15:38:59 +0100

    fix: update modulare-architecture tldr + CORS origin to use environment variable for frontend URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     app.use(cookieParser()); // set the req.cookies
     app.useGlobalPipes(new ValidationPipe({ transform: true })); // validate + run @Transform on dto
     app.enableCors({
-        origin: 'http://localhost:5173',
+        origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
         credentials: true, // necessary for the browser to send/receive cookies
     });
 

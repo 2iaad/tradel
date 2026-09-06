18bc8636d94f5dd2d50aafea9866d7bcd1dd4af3
2iaad <ziadderfoufi77@gmail.com>
2026-09-07 00:33:20 +0100

    [refactor] remove old pg client

--- backend/src/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
-
-@Global() // so i can use DatabaseService without even needing to import it inside other modueles
-@Module({
-    providers: [DatabaseService],
-    exports: [DatabaseService],
-})
-export class DatabaseModule {}

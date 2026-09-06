79cd9148a438b29811dc8c471972bbcda5c539b5
2iaad <ziadderfoufi77@gmail.com>
2026-09-07 00:38:45 +0100

    [refactor] remove pg manual migrations tool

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/env.validation';
-import { DatabaseModule } from './database/database.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { TradesModule } from './trades/trades.module';
 import { NotesModule } from './notes/notes.module';
@@ -17,7 +16,6 @@ import { PrismaModule } from './prisma/prisma.module';
             validate, // nestjs call this func with process.env
         }),
         AuthModule,
-        DatabaseModule,
         AccountsModule,
         TradesModule,
         NotesModule,

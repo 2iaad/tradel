ce26f473469639671eecfd3dfcc1ffd3a410b4be
2iaad <ziadderfoufi77@gmail.com>
2026-09-10 22:01:01 +0100

    [feat(api)]: add helmet security, strict pipe validation, and db health check from NestJS

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'src/generated/prisma/client';
@@ -6,6 +6,8 @@ import type { Env } from 'src/config/env.validation';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    private readonly logger = new Logger(PrismaService.name);
+
     constructor(config: ConfigService<Env>) {
         const connectionString = config.get('dbUrl', { infer: true });
         const adapter = new PrismaPg({ connectionString });
@@ -15,6 +17,17 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
 
     async onModuleInit() {
         await this.$connect();
+        try {
+            await this.$queryRaw`SELECT 1`; // try first query check health
+            this.logger.log('Database connected successfully');
+        } catch (error: any) {
+            this.logger.error(`❌ Database connection failed: ${error.code || error.message}`);
+            this.logger.error(
+                '👉 Make sure PostgreSQL is up and your DB_URL credentials are correct.',
+            );
+
+            // process.exit(1);
+        }
     }
 
     async onModuleDestroy() {
